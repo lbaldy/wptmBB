d4c217bd5672a63cafb9d1bc6c238386ab729378
lukasz <lukasz@baldy.pl>
2016-04-10 00:02:58 +0200

    pseudo trams movement

--- app/scripts/views/HomeView.js
+++ app/scripts/views/HomeView.js
@@ -9,11 +9,14 @@ define(function (require) {
     Template = require("text!../templates/HomeViewTemplate.html"),
     StopsModel = require("../models/StopModel"),
     Config = require("config"),
-    TramsManager = require('../utils/TramManager');
+    TramsManager = require('../utils/TramManager'),
+    TramBase64 = require("text!../../images/trambase64.txt");
 
   var HomeView = BaseAppView.extend({
 
     stopModel: new StopsModel(),
+
+    image: new Image(),
 
     markers: [],
 
@@ -23,8 +26,19 @@ define(function (require) {
       url: "images/tram.png"
     },
 
-    tramImage: {
-      url: "images/tram_v1.png"
+    tramImage: function (angle) {
+      var canvas = document.createElement('canvas');//new Canvas();//document.getElementById("c");
+      var ctx = canvas.getContext("2d");
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.translate(canvas.width / 2, canvas.height / 2);
+      //console.log(angle);
+      ctx.rotate(angle * Math.PI / 180);
+
+      //ctx.fillRect(50,20, -50/2, -20/2);
+
+      ctx.drawImage(this.image, -(this.image.width / 2), -(this.image.height / 2));
+      // ctx.drawImage(this.image, 0, 0);
+      return canvas.toDataURL();
     },
 
     map: undefined,
@@ -38,11 +52,15 @@ define(function (require) {
           ]
         }
       ]
-    },
+    }
+    ,
 
     initialize: function () {
       var options = {};
       var self = this;
+
+
+      self.image.src = TramBase64;
 
       navigator.geolocation.getCurrentPosition(function (position) {
         var lat = position.coords.latitude,
@@ -59,7 +77,16 @@ define(function (require) {
           });
 
           self.markers.push(new google.maps.LatLng(data.lat, data.lon));
-          self.getTrams(data);
+
+          window.setInterval(function () {
+            self.clearTrams();
+            self.getTrams(data)
+          }, 5000);
+
+          window.setInterval(function(){
+            self.animateTrams()
+          }, 100)
+
           self.drawMarkers();
 
           self.map.addListener("click", function (e) {
@@ -80,23 +107,8 @@ define(function (require) {
       });
 
       BaseAppView.prototype.initialize.call(this, options);
-      _.bindAll(this, 'drawTrams', 'drawMarkers', 'getTrams');
+      _.bindAll(this, 'drawTrams', 'drawMarkers', 'getTrams', 'tramImage');
       this.render();
-    },
-
-    getTrams: function (data) {
-      var self = this;
-      self.trams = [];
-      TramsManager.getNearestTrams(data.lat, data.lon, Config.distance).done(function (data) {
-        console.log(data);
-        for(var i = 0; i < data.length; i++){
-          self.trams.push(new google.maps.LatLng(data[i].point.latitude, data[i].point.longitude));
-        }
-        self.drawTrams();
-      }).fail(function (data) {
-        console.log(data);
-      });
-
     },
 
     clearMarkers: function () {
@@ -113,15 +125,55 @@ define(function (require) {
       }
     },
 
-    drawTrams: function(){
+    getTrams: function (data) {
+      var self = this;
+      self.trams = [];
+      TramsManager.getNearestTrams(data.lat, data.lon, Config.distance).done(function (data) {
+        console.log(data);
+        for (var i = 0; i < data.length; i++) {
+          self.trams.push({
+            marker: new google.maps.Marker({
+              position: new google.maps.LatLng(data[i].vehiclePositionData.lat, data[i].vehiclePositionData.lon),
+            }),
+            data: data[i]
+          });
+        }
+        self.drawTrams();
+      }).fail(function (data) {
+        console.log(data);
+      });
+
+    },
+
+    drawTrams: function () {
+      var self = this;
       for (var i = 0; i < this.trams.length; i++) {
-        new google.maps.Marker({
-          position: this.trams[i],
-          icon: this.tramImage,
-          map: this.map
-        });
+        self.trams[i].marker.setMap(self.map);
+        var tramData = self.trams[i].data;
+        self.animateTrams();
+        self.trams[i].marker.addListener("click", function () {
+          console.log(tramData);
+        })
       }
     },
+
+    clearTrams: function () {
+      var self = this;
+      for (var i = 0; i < this.trams.length; i++) {
+        self.trams[i].marker.setMap(null);
+      }
+    },
+
+    animateTrams: function(){
+      // @TODO: Make this properly. It doesn't work and dividing by big number isn't a solution.
+      for (var i = 0; i < this.trams.length; i++) {
+        var sineBearing = (Math.sin(Math.PI * (this.trams[i].data.vehiclePositionData.bearing/180)))/100000;
+        var cosBearing = (Math.cos(Math.PI * (this.trams[i].data.vehiclePositionData.bearing/180)))/100000;
+
+        this.trams[i].marker.setPosition(new google.maps.LatLng(this.trams[i].marker.position.lat() + sineBearing, this.trams[i].marker.position.lng() + cosBearing));
+      }
+    },
+
 
     render: function () {
       var template = _.template(Template)();
